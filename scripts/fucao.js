8011675f67e5f1f5e97169e5bf34b06665163148
Lima152010 <lucaslbr1a2b@gmail.com>
2025-10-26 21:16:07 -0300

    Adicionando uma pagina de sobre Mim

--- scripts/fucao.js
+++
@@ -1,5 +0,0 @@
-
-function conta() {
-    let usuario = document.getElementById("usuario").value;
-    document.getElementById("conta").innerHTML = "<p id='nomeConta'><img src='../imagens/perfil.png'> "+usuario+"</p>"
-}
